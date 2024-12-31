bd30071eb2763f1795e17d6aeffa0ebdb4e054a2
JuraijBinAbdulKhadir <juraijbinabdulkhadir@gmail.com>
2024-12-31 13:32:37 +0530

    service provider boiler plate

---
+++ lib/features/my_services/my_services.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyServicesPage extends StatelessWidget {
+  const MyServicesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: CircularProgressIndicator(),),
+    );
+  }
+}
